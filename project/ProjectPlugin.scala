df770d1d2650af865a35abf003b7fcf9c863217c
fortyseven_degrees <developer@47deg.com>
2022-11-18 05:25:30 +0000

    Update cats-effect to 3.4.1

--- project/ProjectPlugin.scala
+++ project/ProjectPlugin.scala
@@ -12,7 +12,7 @@ object ProjectPlugin extends AutoPlugin {
   object autoImport {
 
     object V {
-      val catsEffect: String    = "3.4.0"
+      val catsEffect: String    = "3.4.1"
       val circe: String         = "0.14.3"
       val doobie: String        = "1.0.0-RC2"
       val fs2: String           = "3.3.0"
