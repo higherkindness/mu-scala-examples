539039623992328539401b3b6c20384913c3b3de
fortyseven_degrees <developer@47deg.com>
2023-01-23 05:35:27 +0000

    Update fs2-core to 3.5.0

--- project/ProjectPlugin.scala
+++ project/ProjectPlugin.scala
@@ -15,7 +15,7 @@ object ProjectPlugin extends AutoPlugin {
       val catsEffect: String    = "3.4.4"
       val circe: String         = "0.14.3"
       val doobie: String        = "1.0.0-RC2"
-      val fs2: String           = "3.4.0"
+      val fs2: String           = "3.5.0"
       val kindProjector: String = "0.13.2"
       val log4cats: String      = "2.5.0"
       val log4s: String         = "1.10.0"
