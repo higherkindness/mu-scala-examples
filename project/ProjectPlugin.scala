e8165cc7ab8021aad3c489a0c0fcfb00b54c5054
fortyseven_degrees <developer@47deg.com>
2023-11-22 05:26:46 +0000

    Revert commit(s) 339c8af

--- project/ProjectPlugin.scala
+++ project/ProjectPlugin.scala
@@ -15,7 +15,7 @@ object ProjectPlugin extends AutoPlugin {
       val catsEffect: String    = "3.5.2"
       val circe: String         = "0.14.6"
       val doobie: String        = "1.0.0-RC4"
-      val fs2: String           = "3.9.3"
+      val fs2: String           = "3.9.2"
       val kindProjector: String = "0.13.2"
       val log4cats: String      = "2.6.0"
       val log4s: String         = "1.10.0"
