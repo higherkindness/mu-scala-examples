7529333f5cf98cb94c6c83ea2ac0b3d4d61c7055
47erbot <developer@47deg.com>
2025-05-28 07:25:23 +0200

    Update log4cats-core, log4cats-slf4j to 2.7.1

--- project/ProjectPlugin.scala
+++ project/ProjectPlugin.scala
@@ -17,7 +17,7 @@ object ProjectPlugin extends AutoPlugin {
       val doobie: String        = "1.0.0-RC5"
       val fs2: String           = "3.12.0"
       val kindProjector: String = "0.13.3"
-      val log4cats: String      = "2.7.0"
+      val log4cats: String      = "2.7.1"
       val log4s: String         = "1.10.0"
       val logback: String       = "1.5.18"
       val mu: String            = "0.33.0"
